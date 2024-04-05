41486ac8c754c754591c4c796abb9850c59dfe6d
AshanHerath <87167360+AshanHerath@users.noreply.github.com>
2024-04-05 12:07:56 +0530

    Create ProductMapper.java

---
+++ src/main/java/com/as/techheaven/productserviceapi/util/mapper/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.as.techheaven.productserviceapi.util.mapper;
+
+import com.as.techheaven.productserviceapi.dto.request.RequestProductDto;
+import com.as.techheaven.productserviceapi.dto.response.ResponseProductDto;
+import com.as.techheaven.productserviceapi.entity.Product;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+
+    ResponseProductDto toResponseProductDto(Product product);
+    Product toProduct(RequestProductDto dto);
+    List<ResponseProductDto> toResponseDoctorDtoList(List<Product> list);
+
+}
