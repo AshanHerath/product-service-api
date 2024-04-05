5bd4d8a2269d4f9fcb510b61e25c5b858ab87272
AshanHerath <87167360+AshanHerath@users.noreply.github.com>
2024-04-05 12:42:10 +0530

    Update ProductRepository.java

--- src/main/java/com/as/techheaven/productserviceapi/repo/ProductRepository.java
+++ src/main/java/com/as/techheaven/productserviceapi/repo/ProductRepository.java
@@ -1,7 +1,16 @@
 package com.as.techheaven.productserviceapi.repo;
 
 import com.as.techheaven.productserviceapi.entity.Product;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query(value = "SELECT * FROM products WHERE product_name LIKE ?1", nativeQuery = true)
+    List<Product> searchProduct(String searchText, PageRequest of);
+
+    @Query(value = "SELECT COUNT(*) FROM products WHERE product_name LIKE ?1", nativeQuery = true)
+    long countProduct(String searchText);
 }
