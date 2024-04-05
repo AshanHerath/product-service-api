a322c72b81f95f86f73cc660ec62fbe2035341f9
AshanHerath <87167360+AshanHerath@users.noreply.github.com>
2024-04-05 12:42:28 +0530

    Create ProductService.java

---
+++ src/main/java/com/as/techheaven/productserviceapi/service/ProductService.java
@@ -0,0 +1,15 @@
+package com.as.techheaven.productserviceapi.service;
+
+import com.as.techheaven.productserviceapi.dto.paginate.PaginateProductResponseDto;
+import com.as.techheaven.productserviceapi.dto.request.RequestProductDto;
+import com.as.techheaven.productserviceapi.dto.response.ResponseProductDto;
+
+public interface ProductService {
+
+    public void createProduct(RequestProductDto dto);
+    public void updateProduct(RequestProductDto dto, long productId);
+    public ResponseProductDto findProduct(long productId);
+    public void deleteProduct(long productId);
+    public PaginateProductResponseDto findAllProducts(int page, int size, String searchText);
+
+}
