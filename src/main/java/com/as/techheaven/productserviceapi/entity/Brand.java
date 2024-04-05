aa4fe57b96ff8751e6f3ae72a4179c97efa83eab
AshanHerath <87167360+AshanHerath@users.noreply.github.com>
2024-04-05 10:52:58 +0530

    Create Brand.java

---
+++ src/main/java/com/as/techheaven/productserviceapi/entity/Brand.java
@@ -0,0 +1,30 @@
+package com.as.techheaven.productserviceapi.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "brands")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Brand {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "brand_name",length = 50, nullable = false)
+    private String name;
+
+    @Lob
+    @Column(name = "brand_imgUrl", nullable = false)
+    private String brandImg;
+
+    @OneToOne
+    @JoinColumn(name = "product_id", unique = true)
+    private Product product;
+
+}
