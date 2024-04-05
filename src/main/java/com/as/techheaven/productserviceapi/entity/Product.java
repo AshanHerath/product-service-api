dc5faa77611aeb25671933fac757693547b2ae67
AshanHerath <87167360+AshanHerath@users.noreply.github.com>
2024-04-05 10:53:08 +0530

    Create Product.java

---
+++ src/main/java/com/as/techheaven/productserviceapi/entity/Product.java
@@ -0,0 +1,56 @@
+package com.as.techheaven.productserviceapi.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Table(name = "products")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private Long id;
+
+    @Column(name = "product_name", nullable = false)
+    private String name;
+
+    @Lob
+    @Column(name = "product_description", nullable = false)
+    private String description;
+
+    @Column(name = "product_quantity", nullable = false)
+    private int quantity;
+
+    @Column(name = "unit_proce", nullable = false)
+    private double price;
+
+    @Column(name = "stock_status")
+    private boolean stockStatus;
+
+    @Column(name = "status")
+    private boolean status;
+
+    @OneToOne(mappedBy = "product", cascade = CascadeType.ALL)
+    private Brand brand;
+
+    @OneToOne(mappedBy = "product", cascade = CascadeType.ALL)
+    private Category category;
+
+    @OneToOne(mappedBy = "product", cascade = CascadeType.ALL)
+    private Discount discount;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    private List<ProductImage> productImages;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    private List<ProductSpecification> specifications;
+
+}
