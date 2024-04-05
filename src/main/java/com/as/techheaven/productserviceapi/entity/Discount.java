c91d6d35325034a899920edd3da3bde1e74361a8
AshanHerath <87167360+AshanHerath@users.noreply.github.com>
2024-04-05 10:53:05 +0530

    Create Discount.java

---
+++ src/main/java/com/as/techheaven/productserviceapi/entity/Discount.java
@@ -0,0 +1,32 @@
+package com.as.techheaven.productserviceapi.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "discounts")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Discount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "discount_title", length = 50, nullable = false)
+    private String title;
+
+    @Column(name = "discount_type", length = 50, nullable = false)
+    private String discountType;
+
+    @Column(name = "discount", nullable = false)
+    private String discount;
+
+    @OneToOne
+    @JoinColumn(name = "product_id", unique = true)
+    private Product product;
+
+}
