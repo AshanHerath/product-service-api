3dc7729ec80a65c3dd7f87fc495355e0d0987f8e
AshanHerath <87167360+AshanHerath@users.noreply.github.com>
2024-04-05 11:25:44 +0530

    Create BrandRepository.java

---
+++ src/main/java/com/as/techheaven/productserviceapi/repo/BrandRepository.java
@@ -0,0 +1,7 @@
+package com.as.techheaven.productserviceapi.repo;
+
+import com.as.techheaven.productserviceapi.entity.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BrandRepository extends JpaRepository<Brand, Long> {
+}
