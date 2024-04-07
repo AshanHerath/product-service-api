ec875e58c5d0fb6a5fe3c173f78ee770c413c94f
AshanHerath <87167360+AshanHerath@users.noreply.github.com>
2024-04-07 11:19:44 +0530

    Create ResponseCategoryDto.java

---
+++ src/main/java/com/as/techheaven/productserviceapi/dto/response/ResponseCategoryDto.java
@@ -0,0 +1,15 @@
+package com.as.techheaven.productserviceapi.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ResponseCategoryDto {
+
+    private long id;
+    private String name;
+
+}
