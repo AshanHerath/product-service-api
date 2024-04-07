8b9a6af8fa5a250b2576c0a1478ce7100489d8c9
AshanHerath <87167360+AshanHerath@users.noreply.github.com>
2024-04-07 11:18:40 +0530

    Create RequestCategoryDto.java

---
+++ src/main/java/com/as/techheaven/productserviceapi/dto/request/RequestCategoryDto.java
@@ -0,0 +1,14 @@
+package com.as.techheaven.productserviceapi.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class RequestCategoryDto {
+
+    private String name;
+
+}
